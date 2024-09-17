5728b69b09ec46489ed095c55306956e0b93b2df
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-17 16:18:44 +0800

    chore: add bundler

---
+++ rollup.config.mjs
@@ -0,0 +1,11 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'cjs'
+	},
+	plugins: [nodeResolve(), commonjs()]
+};
