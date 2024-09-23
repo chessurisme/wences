0f0f0c2acbf8ce38080624c60ae55a9593fc63e7
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-23 17:57:52 +0800

    test(site): experiment tab functionality

--- src/site/index.js
+++ src/site/index.js
@@ -1,79 +1,405 @@
 import '../lib/styles/root.css';
 import '../lib/styles/body.css';
-import { Apple, createIcons, Grape, Menu, Play } from 'lucide';
+import {
+	Apple,
+	ChevronLeft,
+	ChevronRight,
+	createIcons,
+	Grape,
+	Menu,
+	Play,
+	Plus
+} from 'lucide';
 import { Button } from '../lib/components/button/button';
-
-const button = Button({
-	attribute: {
-		class: ' fruit',
-		id: 'pineapple'
-	},
-	behavior: {
-		click: () => console.log('Hi, I am a pineapple!')
-	},
-	content: {
-		text: 'Pineapple'
-	},
-	style: {
-		background: 'orange'
-	}
-});
+import { Tab } from '../lib/components/tab/tab';
 
 document.body.appendChild(
-	Button({
+	Tab({
 		attribute: {
-			class: ' fruit',
-			id: 'apple'
-		},
-		behavior: {
-			onclick: () => console.log('Hi, I am an apple!')
-		},
-		style: {
-			backgroundColor: '#eaeaea'
-		},
-		state: {
-			disabled: false
+			class: 'home-main',
+			id: 'love'
 		},
 		content: {
-			icon: 'apple'
+			type: 'float',
+			index: 2,
+			elements: [
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'apple'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an apple!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'apple',
+						text: 'Apple'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'menu',
+						text: 'Menu'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'apple'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an apple!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'grape',
+						text: 'Grapes'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'play',
+						text: 'Play'
+					}
+				})
+			]
 		}
 	})
 );
 
 document.body.appendChild(
-	Button({
+	Tab({
 		attribute: {
-			class: ' fruit',
-			id: 'grape',
-			title: 'Grape'
-		},
-		behavior: {
-			click: () => console.log('Hi, I am grapes!')
+			class: 'home-main',
+			id: 'love'
 		},
 		content: {
-			icon: 'grape',
-			text: 'Grape'
-		},
-		style: {
-			background: '#eaeaea'
+			type: 'float',
+			index: 2,
+			elements: [
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'apple'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an apple!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'apple'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'menu'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'apple'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an apple!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'grape'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'play'
+					}
+				})
+			]
 		}
 	})
 );
 
 document.body.appendChild(
-	Button({
+	Tab({
 		attribute: {
-			class: ' fruit',
-			id: 'pineapple'
-		},
-		behavior: {
-			click: () => console.log('Hi, I am a pineapple!')
+			class: 'home-main',
+			id: 'envy'
 		},
 		content: {
-			text: 'Play Pineapple'
+			type: 'float',
+			index: 1,
+			elements: [
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'apple'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an apple!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Apple'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Menu'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Preview'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Example'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Courses'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Neuron'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						text: 'Bookmarks'
+					}
+				})
+			]
+		}
+	})
+);
+
+document.body.appendChild(
+	Tab({
+		attribute: {
+			class: 'home-main',
+			id: 'hate'
 		},
-		style: {
-			background: '#eaeaea'
+		content: {
+			type: 'footer',
+			index: 3,
+			elements: [
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'apple'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an apple!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'apple',
+						text: 'Apple'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'menu',
+						text: 'Menu'
+					}
+				}),
+				Button({
+					attribute: {
+						class: ' fruit',
+						id: 'orange'
+					},
+					behavior: {
+						onclick: () => console.log('Hi, I am an orange!')
+					},
+					style: {
+						backgroundColor: '#eaeaea'
+					},
+					state: {
+						disabled: false
+					},
+					content: {
+						icon: 'play',
+						text: 'Play'
+					}
+				})
+			]
 		}
 	})
 );
@@ -83,6 +409,9 @@ createIcons({
 		Apple,
 		Grape,
 		Menu,
-		Play
+		Play,
+		Plus,
+		ChevronLeft,
+		ChevronRight
 	}
 });
