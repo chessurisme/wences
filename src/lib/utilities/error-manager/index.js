8bbc6b384dd26672ca455ca8bc781c441158b84f
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-25 18:59:51 +0800

    docs(utilities): add jsdoc to error management

--- src/lib/utilities/error-manager/index.js
+++ src/lib/utilities/error-manager/index.js
@@ -1,5 +1,14 @@
 import { errorMessages } from './error-messages';
 
+/**
+ * Logs an error message based on the provided error code. If the error message is a function, it is executed with the provided props.
+ * Otherwise, it logs the error message directly. If the error code is not found, it logs an unknown error message.
+ *
+ * @param {string} code - The error code used to retrieve the error message.
+ * @param {Object} [props={}] - Optional properties to pass to the error message function if applicable.
+ *
+ * @returns {void}
+ */
 const logError = (code, props = {}) => {
 	const errorMessage = errorMessages[code];
 
