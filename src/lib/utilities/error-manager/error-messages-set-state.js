2b1654fa103d72bedc1e8ca9601252555ccd6f3c
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-25 21:41:33 +0800

    feat(utilities): add error messages to set state

---
+++ src/lib/utilities/error-manager/error-messages-set-state.js
@@ -0,0 +1,6 @@
+const setStateErrorMessages = {
+	'SS-1':
+		'Invalid parameters: Ensure the first parameter is a DOM element and the second is a non-null object.',
+	'SS-2': (props) =>
+		`Invalid value: The value of "${props.key}" must be a boolean.`
+};
