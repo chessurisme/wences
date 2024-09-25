561d5dbeb55d2bf57c4d9a72aa120b985a327370
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-25 19:13:31 +0800

    docs(utilities): add jsdoc to set attributes

--- src/lib/utilities/set-attributes/index.js
+++ src/lib/utilities/set-attributes/index.js
@@ -2,6 +2,16 @@ import { validateParameters } from '../validate-parameters';
 import { applyAttributes } from './apply-attributes';
 import { logError } from '../error-manager';
 
+/**
+ * Sets attributes on a specified HTML element after validating the parameters. If the parameters are invalid, an error is logged.
+ *
+ * @param {HTMLElement} element - The HTML element to which the attributes will be applied. Must be a valid HTMLElement.
+ * @param {Object} attributes - An object containing key-value pairs of attributes to apply to the element.
+ * @param {string} attributes.key - The attribute name to set on the element.
+ * @param {string} attributes.value - The value to assign to the attribute. Only string values are accepted; non-string values will trigger an error log.
+  
+ * @returns {void}
+ */
 const setAttributes = (element, attributes) => {
 	validateParameters(element, attributes)
 		? applyAttributes(element, attributes)
