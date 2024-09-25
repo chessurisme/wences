f32f8b386799772f93c93878f6bafc22b9013ec7
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-25 21:42:08 +0800

    feat(utilities): concatenate the state error messages to others

--- src/lib/utilities/error-manager/error-messages.js
+++ src/lib/utilities/error-manager/error-messages.js
@@ -1,9 +1,13 @@
 import { generalErrorMessages } from './error-messages-general';
 import { setAttributesErrorMessages } from './error-messages-set-attributes';
+import { setBehaviorErrorMessages } from './error-messages-set-behavior';
+import { setStateErrorMessages } from './error-messages-set-state';
 
 const errorMessages = {
 	...generalErrorMessages,
-	...setAttributesErrorMessages
+	...setAttributesErrorMessages,
+	...setBehaviorErrorMessages,
+	...setStateErrorMessages
 };
 
 export { errorMessages };
