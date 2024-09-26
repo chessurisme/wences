f6edc868e92e6a9ea335b512c7f88aa688f5799e
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-09-26 11:11:15 +0800

    feat(utilities): concatenate the error messages

--- src/lib/utilities/error-manager/error-messages.js
+++ src/lib/utilities/error-manager/error-messages.js
@@ -1,13 +1,17 @@
 import { generalErrorMessages } from './error-messages-general';
 import { setAttributesErrorMessages } from './error-messages-set-attributes';
 import { setBehaviorErrorMessages } from './error-messages-set-behavior';
+import { setContentErrorMessages } from './error-messages-set-content';
 import { setStateErrorMessages } from './error-messages-set-state';
+import { setStyleErrorMessages } from './error-messages-set-style';
 
 const errorMessages = {
 	...generalErrorMessages,
 	...setAttributesErrorMessages,
 	...setBehaviorErrorMessages,
-	...setStateErrorMessages
+	...setStateErrorMessages,
+	...setStyleErrorMessages,
+	...setContentErrorMessages
 };
 
 export { errorMessages };
