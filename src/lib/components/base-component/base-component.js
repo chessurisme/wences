c5299dd8e137daee8fa38a5e61fe8b2989da9c6d
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-09-29 21:00:02 +0800

    refactor(components): separate the typedef

--- src/lib/components/base-component/base-component.js
+++ src/lib/components/base-component/base-component.js
@@ -3,27 +3,7 @@ import { setBehavior } from '../../utilities/set-behavior';
 import { setContent } from '../../utilities/set-content';
 import { setState } from '../../utilities/set-state';
 import { setStyles } from '../../utilities/set-style';
-
-/**
- * @typedef {Object} ComponentConfig
- * @property {Object} [attribute] - An object containing HTML attributes to set on the component.
- * @property {string} [attribute.value] - The value for the HTML attribute (must be a string).
- *
- * @property {Object} [behavior] - An object containing event handlers to set on the component.
- * @property {Function} [behavior.eventType] - The function to call when the specified event occurs (e.g., `click`, `mouseover`).
- *
- * @property {Object} [content] - An object containing content to apply to the component.
- * @property {string|Array<HTMLElement|SVGElement>} [content.value] - The content to set, which can be:
- *    - A string (e.g., text content).
- *    - An array of elements (for multiple DOM elements or SVG elements).
- *    - An icon or an index (for specific content types).
- *
- * @property {Object} [state] - An object containing boolean attributes to apply to the component.
- * @property {boolean} [state.value] - The value for the boolean attribute (true or false).
- *
- * @property {Object} [style] - An object containing styles to apply to the component.
- * @property {string|Object} [style.value] - The value for the CSS property.
- */
+import './component-config.js';
 
 /**
  * Creates a base HTML component with specified configuration options,
