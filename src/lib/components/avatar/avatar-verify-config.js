c8b0614c0caa7df5f0e71473c8a5518878100411
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-09-29 20:27:26 +0800

    feat(utilities): add config verification for an avatar

---
+++ src/lib/components/avatar/avatar-verify-config.js
@@ -0,0 +1,24 @@
+const verifyConfig = (config) => {
+	const conditionList = [
+		{
+			condition: !config,
+			message: 'Invalid input: Config is empty.'
+		},
+		{
+			condition: !config.attribute,
+			message: 'Invalid input: No attribute found in the config.'
+		},
+		{
+			condition: !config.attribute.src,
+			message: 'Invalid input: The src attribute is not found in the config.'
+		}
+	];
+
+	conditionList.forEach((item) => {
+		if (item.condition) {
+			console.error(item.message);
+		}
+	});
+};
+
+export { verifyConfig };
