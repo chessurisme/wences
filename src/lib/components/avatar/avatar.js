47e618b29d2b705e7494cde081efb6b76a9d812c
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-10-02 20:12:22 +0800

    refactor(components): simplify avatar creation

--- src/lib/components/avatar/avatar.js
+++ src/lib/components/avatar/avatar.js
@@ -1,12 +1,15 @@
+import './avatar.css';
 import { verifyConfig } from './avatar-verify-config';
+import { modifyConfig } from './avatar-modify-config';
+import { BaseComponent } from '../base-component';
 
 const Avatar = (config) => {
-	const avatar = createAvatarNode(config);
-	return avatar;
-};
-
-const createAvatarNode = (config) => {
 	if (!verifyConfig(config)) return;
+
+	const modifiedConfig = modifyConfig(config);
+	const avatar = BaseComponent(modifiedConfig, 'img');
+
+	return avatar;
 };
 
 export { Avatar };
