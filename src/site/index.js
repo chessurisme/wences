845b1006b296cc883cfdaddb987f4e21647e170b
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-10-02 20:13:15 +0800

    test(components): experiment avatar

--- src/site/index.js
+++ src/site/index.js
@@ -1,6 +1,29 @@
-import { setAttributes } from '../lib/utilities/set-attributes';
+import { createIcons, Grid } from 'lucide';
+import { Avatar } from '../lib/components/avatar/avatar';
 
-const element = document.createElement('div');
-const attributes = { class: 0 };
-setAttributes(element, attributes);
-document.body.appendChild(element);
+function initializeApp() {
+	const avatar = Avatar({
+		attribute: {
+			class: 'apple',
+			src: '../../assets/favicons/android-chrome-512x512.png',
+			title: 'Avatar'
+		},
+		behavior: {
+			onclick: () => console.log('I am an avatar.')
+		}
+	});
+
+	const elements = [avatar];
+
+	elements.forEach((element) => {
+		document.body.appendChild(element);
+	});
+}
+
+initializeApp();
+
+createIcons({
+	icons: {
+		Grid
+	}
+});
