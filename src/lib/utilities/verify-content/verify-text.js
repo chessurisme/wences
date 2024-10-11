85c11d25bc29d40dfce72424670a8d3e3d398353
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-10-11 15:59:22 +0800

    refactor(utilities): add `name` parameter for dynamic naming of text

--- src/lib/utilities/verify-content/verify-text.js
+++ src/lib/utilities/verify-content/verify-text.js
@@ -2,11 +2,12 @@
  * Verifies if the input text is a valid string.
  *
  * @param {string} text - The text to be verified.
+ * @param {string} [name = 'text'] - The name of string to be verified.
  * @returns {boolean} Returns true if the text is a valid string, otherwise false.
  */
-function verifyText(text) {
+function verifyText(text, name = 'text') {
 	if (text !== undefined && typeof text !== 'string') {
-		console.error('Invalid content: "text" must be a string.');
+		console.error(`Invalid content: "${name}" must be a string.`);
 		return false;
 	}
 
