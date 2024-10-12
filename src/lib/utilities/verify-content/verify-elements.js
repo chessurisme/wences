28c0432f409cae5a37500a7daad7a775b2503422
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-10-12 10:30:45 +0800

    feat(utilities): implement element verification

---
+++ src/lib/utilities/verify-content/verify-elements.js
@@ -0,0 +1,27 @@
+function verifyElements(elements, validTypes) {
+	const isValidType = (element) => {
+		const type = typeof element;
+
+		if (validTypes.includes(type)) {
+			return true;
+		}
+
+		if (element instanceof HTMLElement && validTypes.includes('HTMLElement')) {
+			return true;
+		}
+
+		if (element instanceof SVGElement && validTypes.includes('SVGElement')) {
+			return true;
+		}
+
+		if (Array.isArray(element) && validTypes.includes('Array')) {
+			return true;
+		}
+
+		return false;
+	};
+
+	return elements.every(isValidType);
+}
+
+export { verifyElements };
