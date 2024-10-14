0d1ee574075bfcf1708bb853a6a90bf84ae5d6a3
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-10-14 10:47:36 +0800

    feat(components): implement config verification for toggle list

---
+++ src/lib/components/list/toggle/verify-config.js
@@ -0,0 +1,31 @@
+import { verifyType } from '../../../utilities/verify-content/verify-type';
+
+function verifyConfig(config) {
+	const { content } = config;
+	const { type, checkIndex, items } = content;
+
+	if (!type || verifyType()) {
+		console.error(
+			'Invalid content: "type" must not be empty. Make sure it is set to "checkbox" or "radio".'
+		);
+		return false;
+	}
+
+	if (
+		checkIndex !== undefined &&
+		(!Array.isArray(checkIndex) ||
+			checkIndex.some((index) => typeof index !== 'number'))
+	) {
+		console.error('Invalid content: "checkIndex" must be an array of number.');
+		return false;
+	}
+
+	if (items !== undefined && !Array.isArray(items)) {
+		console.error('Invalid content: "items" must be an array.');
+		return false;
+	}
+
+	return true;
+}
+
+export { verifyConfig };
