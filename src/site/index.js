12872828e1dd699992a03a2148e040a43c180b89
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-10-14 15:45:18 +0800

    test(site): experiment nested list

--- src/site/index.js
+++ src/site/index.js
@@ -9,7 +9,64 @@ function initializeApp() {
 			name: 'jelly',
 			checkIndex: [1],
 			type: 'checkbox',
-			items: ['apple', 'pear', 'grapes', 'date']
+			items: [
+				'pastry',
+				'bread',
+				'sweets',
+				'vegetables',
+				{
+					content: 'fruits',
+					list: ToggleList({
+						attribute: {
+							class: 'list'
+						},
+						content: {
+							name: 'fruits',
+							checkIndex: [],
+							type: 'checkbox',
+							items: [
+								'strawberry',
+								'pomelo',
+								{
+									content: 'orange',
+									list: ToggleList({
+										attribute: {
+											class: 'list'
+										},
+										content: {
+											name: 'fruits',
+											checkIndex: [],
+											type: 'checkbox',
+											items: [
+												'tangelo',
+												'tangerine',
+												{
+													content: 'clementine',
+													list: ToggleList({
+														attribute: { class: 'okay' },
+														content: {
+															name: 'perfect',
+															checkIndex: [2],
+															type: 'radio',
+															items: [
+																'Algerian',
+																'Nules',
+																'Fina',
+																'Clementine di Calabria',
+																'Clementine del Golfo di Taranto'
+															]
+														}
+													})
+												}
+											]
+										}
+									})
+								}
+							]
+						}
+					})
+				}
+			]
 		}
 	});
 
